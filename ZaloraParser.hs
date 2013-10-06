c84b2d5dace1c58726f62557de83c8e56144806f
Salil Wadnerkar <rohshall@gmail.com>
2013-10-06 22:32:45 +0800

    used POSIX regex

--- ZaloraParser.hs
+++ ZaloraParser.hs
@@ -1,14 +1,15 @@
-import Text.HTML.TagSoup
 import Network.HTTP
-import Data.List
-import Data.Maybe
+import Text.Regex.TDFA
 
+extractZaloraLinks :: String -> [String]
+extractZaloraLinks http =
+  let links = getAllTextMatches $ http  =~ "href=\"/([^\"]*)" :: [String]
+  in
+      ["www.zalora.sg" ++ drop 6 link | link <- links]
 
 main :: IO ()
 main = do
   http <- simpleHTTP (getRequest "http://www.zalora.sg/") >>= getResponseBody
-  let tags = parseTags http
-      brandTags = takeWhile (~/= "</ul>") . dropWhile (~/= "<ul id=BrandsSlider>") $ tags
-      links = ["www.zalora.sg/" ++ fromAttrib "href" tag | tag <- brandTags, tag ~== "<a href>"]
+  let links = extractZaloraLinks http
   putStrLn $ show links
   
